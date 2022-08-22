9e78d5e5031d345aa165c458187636a7738d2ae0
MetraVoprak <84026187+MetraVoprak@users.noreply.github.com>
2022-08-22 17:36:41 +0300

    Lesson 2 create frontend (#2)
    lesson 2 ready

--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:3000",
+   'http://127.0.0.1:3000'
+]
 
 # Application definition
 
@@ -41,11 +45,13 @@ INSTALLED_APPS = [
     # my_app
     'rest_framework',
     'userapp',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,7 +135,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # AUTH
-AUTH_USER_MODEL = 'userapp.User'
+# AUTH_USER_MODEL = 'userapp.User'
 
 # DRF
 REST_FRAMEWORK = {
